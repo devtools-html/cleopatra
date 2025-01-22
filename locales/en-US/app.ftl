santo141/gmail

Add a new state property stackChartEqualWidths

locales/en-US/app.ftl
@@ -792,6 +792,7 @@ StackSettings--call-tree-strategy-native-deallocations-sites = Deallocation Site
 StackSettings--invert-call-stack = Invert call stack
     .title = Sort by the time spent in a call node, ignoring its children.
 StackSettings--show-user-timing = Show user timing
+StackSettings--use-stack-chart-equal-widths = Use an equal width for each stack
 
 StackSettings--panel-search =
     .label = Filter stacks:

src/selectors/url-state.js
@@ -78,6 +78,8 @@ export const getLastSelectedCallTreeSummaryStrategy: Selector<
   getProfileSpecificState(state).lastSelectedCallTreeSummaryStrategy;
 export const getShowUserTimings: Selector<boolean> = (state) =>
   getProfileSpecificState(state).showUserTimings;
+export const getStackChartEqualWidths: Selector<boolean> = (state) =>
+  getProfileSpecificState(state).stackChartEqualWidths;
 export const getSourceViewFile: Selector<string | null> = (state) =>
   getProfileSpecificState(state).sourceView.sourceFile;
 export const getSourceViewScrollGeneration: Selector<number> = (state) =>

src/types/actions.js
@@ -523,6 +523,10 @@ type UrlStateAction =
       +showUserTimings: boolean,
     |}
   | {|
+      +type: 'CHANGE_STACK_CHART_EQUAL_WIDTHS',
+      +stackChartEqualWidths: boolean,
+    |}
+  | {|
       +type: 'CHANGE_SHOW_JS_TRACER_SUMMARY',
       +showSummary: boolean,
     |}

src/types/state.js
@@ -374,6 +374,7 @@ export type ProfileSpecificUrlState = {|
   lastSelectedCallTreeSummaryStrategy: CallTreeSummaryStrategy,
   invertCallstack: boolean,
   showUserTimings: boolean,
+  stackChartEqualWidths: boolean,
   committedRanges: StartEndRange[],
   callTreeSearchString: string,
   markersSearchString: string,

devtools-html/cleopatra

locales/en-US/app.ftl
@@ -802,6 +802,7 @@ StackSettings--call-tree-strategy-native-deallocations-sites = Deallocation Site
 StackSettings--invert-call-stack = Invert call stack
     .title = Sort by the time spent in a call node, ignoring its children.
 StackSettings--show-user-timing = Show user timing
+StackSettings--use-stack-chart-same-widths = Use the same width for each stack
 
 StackSettings--panel-search =
     .label = Filter stacks:

src/selectors/url-state.js
@@ -78,6 +78,8 @@ export const getLastSelectedCallTreeSummaryStrategy: Selector<
   getProfileSpecificState(state).lastSelectedCallTreeSummaryStrategy;
 export const getShowUserTimings: Selector<boolean> = (state) =>
   getProfileSpecificState(state).showUserTimings;
+export const getStackChartSameWidths: Selector<boolean> = (state) =>
+  getProfileSpecificState(state).stackChartSameWidths;
 export const getSourceViewFile: Selector<string | null> = (state) =>
   getProfileSpecificState(state).sourceView.sourceFile;
 export const getSourceViewScrollGeneration: Selector<number> = (state) =>

src/types/actions.js
@@ -523,6 +523,10 @@ type UrlStateAction =
       +showUserTimings: boolean,
     |}
   | {|
+      +type: 'CHANGE_STACK_CHART_SAME_WIDTHS',
+      +stackChartSameWidths: boolean,
+    |}
+  | {|
       +type: 'CHANGE_SHOW_JS_TRACER_SUMMARY',
       +showSummary: boolean,
     |}

src/types/state.js
@@ -374,6 +374,7 @@ export type ProfileSpecificUrlState = {|
   lastSelectedCallTreeSummaryStrategy: CallTreeSummaryStrategy,
   invertCallstack: boolean,
   showUserTimings: boolean,
+  stackChartSameWidths: boolean,
   committedRanges: StartEndRange[],
   callTreeSearchString: string,
   markersSearchString: string,